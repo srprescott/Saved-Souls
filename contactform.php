94933beb225546c5414838cf74cb8d9e10f04d05
S R Prescott <sararprescott@yahoo.com>
2019-03-31 12:45:47 -0400

    Draft Contact & Forms Pages

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
     
-    $mailTo = "sararprescott@yahoo.com";
+    $mailTo = "savedsouls@yahoo.com";
     $headers = "Contact From: ".$mailFrom;
     $txt = "You have received an email from ".$name.".\n\n".$message;
     
